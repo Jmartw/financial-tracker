4bc9def157cc012b03df85fe68dcca62c7983645
Jmartw <146871238+Jmartw@users.noreply.github.com>
2023-10-27 13:35:45 -0400

    idk

--- src/main/java/com/pluralsight/FinancialTracker.java
+++ src/main/java/com/pluralsight/FinancialTracker.java
@@ -290,9 +290,7 @@ public class FinancialTracker {
                                                          startDate, LocalDate endDate) {
         // This method filters the transactions by date and prints a report to the console.
         // It takes two parameters: startDate and endDate, which represent the range of dates to filter by.
-        // The method loops through the transactions list and checks each transaction's date against the date range.
-        // Transactions that fall within the date range are printed to the console.
-        // If no transactions fall within the date range, the method prints a message indicating that there are no results.
+        // results.
 
         System.out.println("report ");
         for (Transaction transaction : transactions) {
@@ -306,9 +304,7 @@ public class FinancialTracker {
     private static void filterTransactionsByVendor(String vendor) {
         // This method filters the transactions by vendor and prints a report to the console.
         // It takes one parameter: vendor, which represents the name of the vendor to filter by.
-        // The method loops through the transactions list and checks each transaction's vendor name against the specified vendor name.
-        // Transactions with a matching vendor name are printed to the console.
-        // If no transactions match the specified vendor name, the method prints a message indicating that there are no results.
+        // There no results.
         try {
             for (Transaction transaction : transactions) {
                 if (transaction.getVendor().equalsIgnoreCase(vendor)) {
